2933d3ce5b13100438056bb6c6422cf678158efe
13kritan <13kritan@gmail.com>
2025-12-28 04:56:35 +0545

    auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -27,6 +27,7 @@ const registerUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
+  console.log(email, password)
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
     res.json({
