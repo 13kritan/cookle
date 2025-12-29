b5fb234b55abac885d749bf16c0b4d2fb856248a
13kritan <13kritan@gmail.com>
2025-12-30 02:38:42 +0545

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,9 +12,10 @@ const app = express();
 
 
 const allowedOrigins = [
-  "http://localhost:3000",                   // local dev
-  "https://cookle.netlify.app",       // your live frontend URL (Netlify or Vercel)
-  "https://cookle-har5.onrender.com" // if testing frontend on Render
+  "http://localhost:3000", // local dev
+  "https://cookle-f.vercel.app"
+  "https://cookle.netlify.app",       // live frontend URL (Netlify or Vercel)
+  "https://cookle-har5.onrender.com" //  on Render
 ];
 
 app.use(cors({
